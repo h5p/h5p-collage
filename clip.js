e9af26acfe54019f81adeb35d8f24fde6c64356d
Gwenillia <55385809+Gwenillia@users.noreply.github.com>
2025-08-13 14:15:09 +0200

    VA-1040 Redesign collage placeholder editor (#77)
    * VA-1040 only use placeholder in view. Reput the empty class
    
    * VA-1040 put back return to remove empty class

--- clip.js
+++ clip.js
@@ -70,10 +70,15 @@
      * Triggers the loading of the image.
      */
     self.load = function () {
-      // No image set, use placeholder
-      if (self.empty()) {
+      // No image set, use placeholder only if H5PEditor is not defined. Else use empty class.
+      if (self.empty() && typeof H5PEditor === 'undefined') {
         self.$wrapper[0].appendChild(H5P.Components.PlaceholderImg());
         return;
+      } else if (self.empty()) {
+        self.$wrapper.addClass('h5p-collage-empty');
+        return;
+      } else {
+        self.$wrapper.removeClass('h5p-collage-empty');
       }
 
       // Create image
